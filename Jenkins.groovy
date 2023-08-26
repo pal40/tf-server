639c1508614cbf83541c0c8877800f8b3a3e1a6c
meet.me.pal.1980 <meet.me.pal.1980@gmail.com>
2023-08-26 21:15:18 +0800

    .

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -10,22 +10,6 @@ pipeline {
                 checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/pal40/tf-server']])
             }
         }
-// Stage - 2 to get AWS Credentials
-//
-        stage("AWS Cred") {
-            steps {
-                    withCredentials([string(credentialsId: 'aws-jenkins', variable: 'secret')]) {
-                        script {
-                        def creds = readJSON text: secret
-                        env.AWS_ACCESS_KEY_ID = creds['accessKeyId']
-                        env.AWS_SECRET_ACCESS_KEY = creds['secretAccessKey']
-                        env.AWS_REGION = 'ap-southeast-1'
-                                }   
-        sh "aws sts get-caller-identity"
-    }
-            }
-        }
-
 // Stage - 3 to init terraform
 //
         stage("Terrform Init") {
