a2be37ddf1e467fb46b3584eac161ffc0942e4f9
pal40 <pal40.sg@gmail.com>
2023-08-26 21:50:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,5 +8,7 @@ terraform {
         key = "main"
         region = "ap-southeast-1"
         dynamodb_table = "tf-server-backend-dynamodb"
+        profile = "default"
+        role_arn = "arn:aws:iam::064231883143:role/tf-project-role"
     }
 }
