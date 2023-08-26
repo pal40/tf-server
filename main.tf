9b49aa06716461c5465a20410e4f9a6351ea1f1e
meet.me.pal.1980 <meet.me.pal.1980@gmail.com>
2023-08-26 18:33:43 +0800

    Added S3 & DynamoDB as backend

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
     region =  "ap-southeast-1"  # Asia Pacific (Singapore)
 }
+
+terraform {
+    backend "s3" {
+        bucket = "tf-server-backend-26aug2023"
+        key = "main"
+        region = "ap-southeast-1"
+        dynamodb_table = "tf-server-backend-dynamodb"
+    }
+}
